342f8fb98034233464aa55aa6615fa8205bce3ae
Rocky Breslow <1774125+rbreslow@users.noreply.github.com>
2022-06-21 22:10:41 -0400

    Add cruise climb profile for the Dakota
    This performance profile assumes the pilot is flying at 100 KIAS (as
    recommended by the POH) for a balance between engine cooling and climb
    performance. After 100 KIAS will no longer sustain 200 FPM, search for
    the next airspeed that can maintain that climb rate. If no airspeed
    satisfies 200 FPM, target a climb rate of 190 FPM and perform the search
    again. Rinse and repeat until the airplane can't climb anymore (cruise
    climb intersects Vy at the absolute ceiling).

---
+++ examples/dakota_performance/cruise_climb.py
@@ -0,0 +1,66 @@
+import numpy as np
+import numpy.typing as npt
+
+from the_bootstrap_approach.conditions import FullThrottleConditions
+from the_bootstrap_approach.dataplate import DataPlate
+from the_bootstrap_approach.equations import density_altitude
+from the_bootstrap_approach.mixture import Mixture
+from the_bootstrap_approach.performance import (
+    bootstrap_cruise_performance_table,
+    by_altitude_profile,
+    PerformanceProfile,
+    ByKCASRowIndex,
+)
+
+
+def cruise_climb(
+    dataplate: DataPlate, gross_aircraft_weight: float, isa_diff: float = 0
+) -> PerformanceProfile:
+    def func(pressure_altitude: float, oat_f: float) -> npt.NDArray[np.float64]:
+        mixture = Mixture.BEST_POWER
+
+        # Below 5,000' DA (~85% Power), we need to use a full rich mixture.
+        if density_altitude(pressure_altitude, oat_f) < 5000:
+            mixture = Mixture.FULL_RICH
+
+        operating_conditions = FullThrottleConditions(
+            dataplate,
+            gross_aircraft_weight,
+            pressure_altitude,
+            oat_f,
+            mixture,
+            dataplate.rated_full_throttle_engine_rpm,
+        )
+
+        row = bootstrap_cruise_performance_table(
+            dataplate, operating_conditions, 100, 101, 1
+        )[0]
+
+        target_airspeed = 100
+        target_fpm = 200
+
+        while row[ByKCASRowIndex.RATE_OF_CLIMB] < target_fpm:
+            row = bootstrap_cruise_performance_table(
+                dataplate, operating_conditions, target_airspeed - 1, target_airspeed, 1
+            )[0]
+            target_airspeed -= 1
+
+            # Once it's impossible to climb at this target rate, set a lower
+            # target and reset the search.
+            if row[ByKCASRowIndex.KCAS] == 50:
+                target_airspeed = 100
+                target_fpm -= 10
+
+            # We've hit our ceiling.
+            if target_fpm <= 0:
+                break
+
+        return row
+
+    return PerformanceProfile(
+        f"100 KIAS Cruise Climb {gross_aircraft_weight} lbf, ISA{isa_diff:+} ℃",
+        dataplate,
+        gross_aircraft_weight,
+        isa_diff,
+        by_altitude_profile(func, isa_diff),
+    )
